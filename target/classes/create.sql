8bbe5bf1898d2f62a1bef94698b6830d86d5dbef
luismiguelviewnext <135017297+luismiguelviewnext@users.noreply.github.com>
2023-10-04 17:24:26 +0200

    cambios

--- target/classes/create.sql
+++ target/classes/create.sql
@@ -1,27 +1,27 @@
 CREATE SCHEMA `bd_productospedidos` ;
 
 CREATE TABLE `bd_productospedidos`.`productos` (
-  `codigoProducto` INT NOT NULL,
+  `codigo_producto` INT NOT NULL,
   `producto` VARCHAR(45) NOT NULL,
-  `precioUnitario` DOUBLE NOT NULL,
+  `precio_unitario` DOUBLE NOT NULL,
   `stock` INT NOT NULL,
-  PRIMARY KEY (`codigoProducto`),
-  UNIQUE INDEX `codigoProducto_UNIQUE` (`codigoProducto` ASC) VISIBLE);
+  PRIMARY KEY (`codigo_producto`),
+  UNIQUE INDEX `codigo_producto_UNIQUE` (`codigo_producto` ASC) VISIBLE);
 
 
   CREATE TABLE `bd_productospedidos`.`pedidos` (
-  `idPedido` INT NOT NULL,
-  `codigoProducto` INT NOT NULL,
+  `id_pedido` INT NOT NULL,
+  `codigo_producto` INT NOT NULL,
   `unidades` INT NOT NULL,
   `total` DOUBLE NOT NULL,
   `fecha` DATETIME NULL,
-  PRIMARY KEY (`idPedido`),
-  UNIQUE INDEX `idPedido_UNIQUE` (`idPedido` ASC) VISIBLE);
+  PRIMARY KEY (`id_pedido`),
+  UNIQUE INDEX `id_pedido_UNIQUE` (`idPedido` ASC) VISIBLE);
 
 
 ALTER TABLE `bd_productospedidos`.`pedidos` 
-ADD CONSTRAINT `codigoProducto`
+ADD CONSTRAINT `codigo_producto`
   FOREIGN KEY (`idPedido`)
-  REFERENCES `bd_productospedidos`.`productos` (`codigoProducto`)
+  REFERENCES `bd_productospedidos`.`productos` (`codigo_producto`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
